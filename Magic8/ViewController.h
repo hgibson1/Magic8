a9bc27f7f07f0f6bc86e165bdedc9791e92849d6
Hannah Gibson <hgibson1@bu.edu>
2014-08-01 22:14:09 -0400

    First Commit!

--- Magic8/ViewController.h
+++ Magic8/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property(nonatomic, weak) IBOutlet UITextField *QuestionLine;
+@property(nonatomic, weak) NSString *Question;
 
 @end
