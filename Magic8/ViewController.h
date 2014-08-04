f7e2d038f2f8f29c2e7e2b0ff542370abd9a1ca8
Hannah Gibson <hgibson1@bu.edu>
2014-08-04 19:16:45 -0400

    Fixed view controller ;)

--- Magic8/ViewController.h
+++ Magic8/ViewController.h
@@ -9,7 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-@property(nonatomic, weak) IBOutlet UITextField *QuestionLine;
-@property(nonatomic, weak) NSString *Question;
 
 @end
